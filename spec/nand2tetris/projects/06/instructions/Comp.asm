3c9186d2a7c046e0f01d5d2da92872095ef53727
Janet Davis <davisj@whitman.edu>
2019-01-06 20:09:26 -0800

    Refactored phase 1 test cases

--- spec/nand2tetris/projects/06/instructions/Comp.asm
+++ spec/nand2tetris/projects/06/instructions/Comp.asm
@@ -1,7 +1,7 @@
 // Copyright Janet Davis, January 2018.
 // Free for educational use.
 
-// Exercises all computations on the A and D registers.
+// Exercises all computation types.
 // This is not a program and cannot be tested by running the resulting
 // .hack file.  Instead, compare to the .hack file produced by the 
 // given assembler.
@@ -24,3 +24,13 @@ D=D-A
 D=A-D
 D=D&A
 D=D|A
+D=M
+D=!M
+D=-1
+D=M+1
+D=M-1
+D=D+M
+D=D-M
+D=M-D
+D=D&M
+D=D|M
--- spec/nand2tetris/projects/06/instructions/Jump.asm
+++ spec/nand2tetris/projects/06/instructions/Jump.asm
@@ -1,7 +1,7 @@
 // Copyright Janet Davis, January 2018.
 // Free for educational use.
 
-// Exercises all possible jumps.
+// Exercises all possible jumps, plus combining with computations and assignment.
 // This is not a program and cannot be tested by running the resulting
 // .hack file.  Instead, compare to the .hack file produced by the 
 // given assembler.
@@ -14,3 +14,5 @@ D;JLT
 D;JNE
 D;JLE
 0;JMP
+D-1;JGT
+D=D-1;JGT
