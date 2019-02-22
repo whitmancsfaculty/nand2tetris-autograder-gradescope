ddc41dbc2b402b764dcd3f9b7adc6175cc61977c
Janet Davis <davisj@whitman.edu>
2019-02-22 09:04:23 -0800

    Removed tests relying on 0x 0b formatitng of numbers, which did not work so was confusing

--- spec/nand2tetris/projects/06/instructions/AInstruction.asm
+++ spec/nand2tetris/projects/06/instructions/AInstruction.asm
@@ -1,12 +1,12 @@
 // Copyright Janet Davis, January 2018.
 // Free for educational use.
 
-// Exercises only the A-instruction type.
+// Exercises only the A-instruction type, with no symbols.
 // This is not a program and cannot be tested by running the resulting
 // .hack file.  Instead, compare to the .hack file produced by the 
 // given assembler.
 
-@5
-@10
-@1024
-@24575
+@0
+@1
+@42
+@12345
