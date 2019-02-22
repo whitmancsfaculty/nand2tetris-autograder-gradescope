248b158ca3f80be66f64452b6edb58e933807519
Janet Davis <davisj@whitman.edu>
2019-02-22 09:03:18 -0800

    Added explanatory comment

--- spec/nand2tetris/projects/06/max/Max.asm
+++ spec/nand2tetris/projects/06/max/Max.asm
@@ -4,6 +4,7 @@
 // File name: projects/06/max/Max.asm
 
 // Computes R2 = max(R0, R1)  (R0,R1,R2 refer to  RAM[0],RAM[1],RAM[2])
+// Tests use of multiple labels in a conditional
 
    @R0
    D=M              // D = first number
