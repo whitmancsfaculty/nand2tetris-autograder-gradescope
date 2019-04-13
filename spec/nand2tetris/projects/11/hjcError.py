e53e779135ffaf262124d8e1247988b5206f67af
Janet Davis <davisj@whitman.edu>
2019-04-13 13:51:26 -0700

    First cut at project 10 autograder

--- spec/nand2tetris/projects/11/hjcError.py
+++
@@ -1,8 +0,0 @@
-"""
-hjcError.py -- Error exceptions for Hack Jack compiler
-Solution provided by Nand2Tetris authors, licensed for educational purposes
-"""
-
-class HjcError(Exception):
-    pass
-    
